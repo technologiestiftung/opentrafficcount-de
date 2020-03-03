3083d6079eb95648af95326ca0a77da59d2ab648
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-03 17:37:55 +0100

    chore(remove test code)

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,27 +1,28 @@
 import { add } from "./lib/stub";
 
-let count = 0;
+// let count = 0;
 document.addEventListener("DOMContentLoaded", function() {
+  console.log(`2 + 2 = ${add({ a: 2, b: 3 })}`);
   // setInterval(() => {
-  count = add({ a: count, b: 1 });
-  console.log(count);
-  // }, 1000);
+  // count = add({ a: count, b: 1 });
+  // console.log(count);
+  // // }, 1000);
 
-  console.log(
-    "Array map",
-    [1, 2, 3].map(elem => elem + 1)
-  );
-  console.log("Object keys", Object.keys({ foo: 1, bah: 2 }));
+  // console.log(
+  //   "Array map",
+  //   [1, 2, 3].map(elem => elem + 1)
+  // );
+  // console.log("Object keys", Object.keys({ foo: 1, bah: 2 }));
 
-  const p = new Promise(resolve => {
-    setTimeout(() => {
-      resolve(true);
-    }, 5000);
-  });
+  // const p = new Promise(resolve => {
+  //   setTimeout(() => {
+  //     resolve(true);
+  //   }, 5000);
+  // });
 
-  p.then(res => {
-    console.log("Promise resolved", res);
-  });
+  // p.then(res => {
+  //   console.log("Promise resolved", res);
+  // });
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   // if (window.LiveReload !== undefined) {
--- src/index.md
+++ src/index.md
@@ -5,5 +5,3 @@ title: Hello World
 
 This is content Hello Open Traffic Count
 {: .content}
-
-Foo
